17bd8427107ef2b00d04d4ecd690ca78a79e8c5c
Sergey Matveychuk <sem@semmy.ru>
2025-04-19 18:19:31 +0300

    Print remark messages in stderr. Print important info in stdout.

--- parse-ygg-peers.sh
+++ parse-ygg-peers.sh
@@ -2,6 +2,10 @@
 
 # apt install curl fping
 
+function echo_stderr {
+	echo $* > /dev/stderr
+}
+
 # defaults
 PING_COUNT=5
 RESULT_NUMBER=0
@@ -14,7 +18,7 @@ while [[ "$#" -gt 0 ]]; do
         -n|--num) RESULT_NUMBER="$2"; shift ;;
         -d|--debug) DEBUG=1 ;;
 	-s|--script) SCRIPT_READY=1 ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+        *) echo_stderr "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
@@ -25,21 +29,21 @@ content=$(curl -sSf https://raw.githubusercontent.com/yggdrasil-network/public-p
 for h in $(echo "$content"|awk '/(tcp|tls|quic|ws):\/\/([-a-zA-Z0-9\.:\[\]]+):[0-9]+/ {print gensub(/\s+\* `(tcp|tls|quic|ws):\/\/\[?([-a-zA-Z0-9\.:]+)\]?:[0-9]+.*`.*$/, "\\2", 1)}' | sort -u); do
 	hosts+=($h)
 done
-echo Found ${#hosts[@]} public peers
+echo_stderr Found ${#hosts[@]} public peers
 
 i=0
 for host in ${hosts[@]}; do
 	i=$((i+1))
 	if [ $DEBUG -eq 1 ]; then
-		echo $i: ping $host
+		echo_stderr $i: ping $host
 	fi
 	out=$(fping -c $PING_COUNT -q -m -A $host 2>&1|grep -v duplicate)
 	retVal=$?
 	if [ $retVal -ne 0 ]; then
 		if [ $DEBUG -eq 1 ]; then
-			echo "====> Peer $host is dead"
+			echo_stderr "====> Peer $host is dead"
 		else
-			echo -n x
+			echo_stderr -n x
 		fi
 		continue
 	fi
@@ -52,37 +56,37 @@ for host in ${hosts[@]}; do
 		avgrtt=$(echo "$o"| awk '{print gensub(/^.*[0-9.]+\/([0-9.]+)\/[0-9.]+.*$/, "\\1", 1, $8)}')
 		if [ $loss -eq 0 ]; then
 			if [ $DEBUG -eq 1 ]; then
-				echo "$ip : rtt $avgrtt"
+				echo_stderr "$ip : rtt $avgrtt"
 			else
-				echo -n .
+				echo_stderr -n .
 			fi
 			result="${result}$host ($ip) $avgrtt
 "
 		else
 			if [ $loss -eq 100 ]; then
 				if [ $DEBUG -eq 1 ]; then
-					echo "===> $ip is unreachable."
+					echo_stderr "===> $ip is unreachable."
 				else
-					echo -n u
+					echo_stderr -n u
 				fi
 			else
 				if [ $DEBUG -eq 1 ]; then
-					echo "===> $ip has $loss% lost packets. Remove it."
+					echo_stderr "===> $ip has $loss% lost packets. Remove it."
 				else
-					echo -n b
+					echo_stderr -n b
 				fi
 			fi
 		fi
 	done
-	if [ $(($i%5)) -eq 0 ]; then
-		echo -n $i
+	if [ $DEBUG -eq 0 -a $(($i%5)) -eq 0 ]; then
+		echo_stderr -n $i
 	fi
 done
 
-echo
+echo_stderr
 
 if [ $DEBUG -eq 1 ]; then
-	echo "================== SORTED RESULTS =================="
+	echo_stderr "================== SORTED RESULTS =================="
 fi
 
 sorted=$(echo "$result"|grep -v '^$'|sort -n -k3)
