127b74982f8e6d0b1f9784180ee166ad94b41cbc
Sergey Matveychuk <sem@semmy.ru>
2026-06-14 14:19:10 +0300

    - Run as root if does not - Fix regexp for SOA - Fix SOA increment - Thaw instead of reload

--- sign-zone.sh
+++ sign-zone.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+        echo "Restart as root"
+        sudo $0
+        exit
+fi
 
 curdir=`dirname $0`
 cd $curdir
+
+echo Freeze dynamic updates
+/usr/sbin/rndc freeze semmy.ru
 
 echo Generating TLSA records...
 /usr/local/bin/generate_tlsa.py generate_tlsa.txt > semmy.ru-tlsa.inc
@@ -13,16 +22,21 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-curr_soa=`awk '/\t*(\d*)\t*; serial \(YYYYMMDDNN\)/ {print $1}' semmy.ru`
+curr_soa=`awk '/\t*([0-9]*)\t*; serial \(YYYYMMDDNN\)/ {print $1}' semmy.ru`
 curr_date=`date +'%Y%m%d00'`
 
-if [ "$curr_soa" -eq "$curr_date" ]; then
-        curr_date=$((curr_date+1))
+if [ "$curr_soa" -ge "$curr_date" ]; then
+        curr_date=$((curr_soa+1))
 fi
 sed -i 's#[[:space:]]*[[:digit:]]*[[:space:]]*; serial (YYYYMMDDNN)#\t\t'$curr_date'\t; serial (YYYYMMDDNN)#' semmy.ru
 
 /usr/bin/dnssec-signzone -S -N keep semmy.ru
 echo "Zone semmy.ru signed"
 
-/usr/sbin/rndc reload 2> /dev/null
+# Bellow thaw command reload zone too
+#/usr/sbin/rndc reload 2> /dev/null
+
+echo Thaw dynamic updates
+/usr/sbin/rndc thaw semmy.ru
+
 exit 0
